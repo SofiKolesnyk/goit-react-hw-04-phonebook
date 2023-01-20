8c0362dff41df956a6bdcfdbc30e123b7ffee4c4
SofiKolesnyk <sofi_serg@ukr.net>
2023-01-20 20:06:16 +0200

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
+import { useEffect, useRef, useState } from 'react';
+import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix';
+import Section from './Section';
+import ContactForm from './ContactForm';
+import Contacts from './Contacts';
+
+const LOCAL_KEY = 'phonebookContacts';
+
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const didRender = useRef(false);
+  useEffect(() => {
+    let localContacts = localStorage.getItem(LOCAL_KEY);
+    localContacts = localContacts ? JSON.parse(localContacts) : [];
+    setContacts([...localContacts]);
+  }, []);
+
+  useEffect(() => {
+    if (!didRender.current) {
+      didRender.current = true;
+      return;
+    }
+    window.localStorage.setItem(LOCAL_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  const onGetDataForm = (data) => {
+    const hasName = contacts.some(it => it.name === data.name);
+    if (hasName) {
+      Notify.warning (`Contact "${data.name}" is already exist.`);
+      return;
+    }
+
+    setContacts(p => [...p, { ...data, id: nanoid() }]);
+  };
+
+  const deleteItem = (deletedId) => {
+    setContacts(p => p.filter(({ id }) => id !== deletedId));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Section title='Phonebook'>
+        <ContactForm
+          onSubmit={onGetDataForm}
+        />
+      </Section>
+      <Section title='Contacts'>
+        <Contacts
+          contacts={contacts}
+          onClickDelete={deleteItem}
+        />
+      </Section>
     </div>
   );
-};
+}
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import s from './Contacts.module.css';
+import InputField from '../InputField';
+import Notification from '../Notification';
+
+function Contacts({contacts, onClickDelete}) {
+  const [filter, setFilter] = useState('');
+
+  const handleInputChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const filteredContacts = contacts.filter(it => it.name.includes(filter));
+
+  return (
+    <div className={s.container}>
+      <InputField
+        label='Find contacts by name'
+        value={filter}
+        onChange={handleInputChange}
+        type='text'
+        name='filter'
+      />
+
+      {!filteredContacts.length
+        ? <Notification message='Contact list is empty.' />
+        : <ul className={s.list}>
+          {filteredContacts.map(({ id, name, number }) => (
+            <li key={id} className={s.item}>
+              <span className={s.name}>{name}</span>
+              <span>{number}</span>
+              <button
+                type='button'
+                onClick={() => onClickDelete(id)}
+              >Delete
+              </button>
+            </li>
+          ))}
+        </ul>}
+    </div>
+  );
+}
+
+Contacts.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+  })).isRequired,
+  onClickDelete: PropTypes.func.isRequired,
+};
+
+export default Contacts;
